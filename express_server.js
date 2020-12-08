02c1fea4e007651de4ce4ba2eebfe26db620b072
Cameron <camglowicki@hotmail.com>
2020-12-08 04:47:44 +0000

    added redirect for new post to URLs

--- express_server.js
+++ express_server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// body parser
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// set view engine
 app.set('view engine', 'ejs');
 
+// generate UID
 function generateRandomString() {
   let result = '';
   const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -17,14 +20,23 @@ function generateRandomString() {
   return result;
 };
 
+// port and database info
+app.listen(PORT, () => {
+  console.log(`The app is listening on port ${PORT}...`)
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com" // for (let url in urls) --> 9sm5xK = url --> value is google.com (urls)
 };
 
-// homepage
+// homepage stuff
 app.get('/', (req, res) => {
   res.send('Hello there, and welcome!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World!</b></body></html>\n');
 });
 
 // urls list
@@ -33,29 +45,23 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.post('/urls', (req, res) => { //specificity!!??
-  res.send(generateRandomString());
+app.post('/urls', (req, res) => { 
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`urls/${shortURL}`);
 });
 
+// new urls functionality
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
-// get info...?
-app.get('/urls/:shortURL', (req, res) => { //.get called route or route handler
-  console.log(req);
+// get info 
+app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World!</b></body></html>\n');
-});
-
-app.listen(PORT, () => {
-  console.log(`The app is listening on port ${PORT}...`)
 });
