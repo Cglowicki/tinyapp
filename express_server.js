4538dc71dc4f23e7ce1174d5e134b64ff4a592a4
Cameron <camglowicki@hotmail.com>
2020-12-11 05:31:21 +0000

    touched up for code legibility and site sensibility

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-// setup
+// setup && file imports
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -23,10 +23,10 @@ app.use(cookieSession({
 app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
-  console.log(`The app is listening on port ${PORT}...`)
+  console.log(`The app is listening on port ${PORT}...`);
 });
 
-//database info
+// database info
 const users = {
   "84Yu54": {
     id: "84Yu54",
@@ -41,26 +41,26 @@ const users = {
 };
 
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "84Yu54" }, 
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "84Yu54" },
   "9sm5xK": { longURL: "http://www.google.com", userID: "OnF68t" }
 };
 
-//route handlers
+// route handlers
 app.get('/', (req, res) => {
-
+// render homepage
   const id = req.session['id'];
   const user = users[id];
-  const templateVars = { user }
+  const templateVars = { user };
   res.render('homepage', templateVars);
 });
 
 app.get('/urls', (req, res) => {
-
+// render urls specific to user
   const id = req.session['id'];
   const user = users[id];
   const myURLS = userURL(id, urlDatabase);
-  console.log('MY URLS!!', myURLS);
-  const templateVars = { 
+
+  const templateVars = {
     user,
     urls: myURLS
   };
@@ -73,7 +73,7 @@ app.get('/register', (req, res) => {
   const id = req.session['id'];
   const user = users[id];
 
-  const templateVars = { 
+  const templateVars = {
     user,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
@@ -95,10 +95,10 @@ app.get('/urls/new', (req, res) => {
     
   const id = req.session['id'];
   const user = users[id];
-
+  // only logged in users have permission
   if (id === undefined) {
     res.redirect('/login');
-  };
+  }
 
   const templateVars = { user };
   res.render('urls_new', templateVars);
@@ -109,7 +109,7 @@ app.get('/urls/:shortURL', (req, res) => {
   const id = req.session['id'];
   const user = users[id];
 
-  const templateVars = { 
+  const templateVars = {
     user,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL
@@ -129,45 +129,48 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.post('/urls', (req, res) => { 
-
+app.post('/urls', (req, res) => {
+  
   const userID = req.session['id'];
-  const longURL = req.body.longURL
+  const longURL = req.body.longURL;
   const shortURL = dataHelpers.generateRandomString();
+  // user specific object formed in users database
   urlDatabase[shortURL] = {longURL, userID};
   
   res.redirect(`urls/${shortURL}`);
 });
 
-app.post('/register', (req, res) => {  // some stuff for error messages
+app.post('/register', (req, res) => {
   
   const id = dataHelpers.generateRandomString();
 
   const email = req.body.email;
-  const password = req.body.password; //bcrypt
+  const password = req.body.password;
   const hashPass = bcrypt.hashSync(password, 2);
 
+  // emailLookup() looping through user object, see dataHelpers.js
+  if (email === '' || password === '') {
+    return res.status(400).send('Whoops! One or more fields was left blank...');
+  }
   if (dataHelpers.emailLookup(email, users)) {
     return res.status(400).send('Hmm... Looks like that email has been registered');
   }
-  if (email === '' || password === '') { //may not be nothing with hash!!!!!!!!!!!
-    return res.status(400).send('Whoops! One or more fields was left blank...');
-  }
   
   users[id] =  { id, email, password: hashPass };
-  req.session.id = id; // HERE
+  req.session.id = id;
   res.redirect('/urls');
 });
 
 app.post('/login', (req, res) => {
 
-  const password = req.body.password; //bcrypt
-  
+  const password = req.body.password;
   const email = req.body.email;
+
+  // emailLookup() returns user object, see dataHelper.js
   const userInfo = dataHelpers.emailLookup(email, users);
   
   if (bcrypt.compareSync(password, userInfo.password)) {
-    req.session.id = userInfo.id; // AND HERE
+    req.session.id = userInfo.id;
     res.redirect('/urls');
   } else {
     return res.status(401).send('Wrong email or password, try again.');
@@ -175,9 +178,9 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-
+// logout redirects to homepage
   req.session = null;
-  res.redirect('/urls');
+  res.redirect('/');
 });
 
 app.post('/urls/:shortURL/edit', (req, res) => {
